08edbd6beb78d6daf95bf807f58f0b45d63a302c
Rocky Warren <rocky@clipboardhealth.com>
2026-05-17 14:27:14 -0500

    chore: move groundcrew package to repo root

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { createVitestConfig } from "../../vitest.shared.ts";
+import { createVitestConfig } from "./vitest.shared.ts";
 
 export default createVitestConfig({
   importMetaUrl: import.meta.url,
